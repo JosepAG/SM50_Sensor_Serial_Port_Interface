56f3fcc8307193dc65a58ae2516b3c0b4ade7983
sanglic <josepengineer@gmail.com>
2020-07-24 14:35:43 +0200

    final

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,29 +29,244 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title3 = new System.Windows.Forms.DataVisualization.Charting.Title();
             this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.data_tb = new System.Windows.Forms.TextBox();
+            this.start_btn = new System.Windows.Forms.Button();
+            this.stop_btn = new System.Windows.Forms.Button();
+            this.save_btn = new System.Windows.Forms.Button();
+            this.value_pb = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
-            // serialPort1
+            // data_tb
             // 
-            this.serialPort1.PortName = "COM19";
-            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            this.data_tb.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.data_tb.Location = new System.Drawing.Point(788, 374);
+            this.data_tb.Multiline = true;
+            this.data_tb.Name = "data_tb";
+            this.data_tb.ReadOnly = true;
+            this.data_tb.Size = new System.Drawing.Size(250, 64);
+            this.data_tb.TabIndex = 0;
+            // 
+            // start_btn
+            // 
+            this.start_btn.Location = new System.Drawing.Point(788, 129);
+            this.start_btn.Name = "start_btn";
+            this.start_btn.Size = new System.Drawing.Size(250, 49);
+            this.start_btn.TabIndex = 1;
+            this.start_btn.Text = "Start";
+            this.start_btn.UseVisualStyleBackColor = true;
+            this.start_btn.Click += new System.EventHandler(this.start_btn_Click);
+            // 
+            // stop_btn
+            // 
+            this.stop_btn.Location = new System.Drawing.Point(787, 205);
+            this.stop_btn.Name = "stop_btn";
+            this.stop_btn.Size = new System.Drawing.Size(251, 49);
+            this.stop_btn.TabIndex = 2;
+            this.stop_btn.Text = "Stop";
+            this.stop_btn.UseVisualStyleBackColor = true;
+            this.stop_btn.Click += new System.EventHandler(this.stop_btn_Click);
+            // 
+            // save_btn
+            // 
+            this.save_btn.Location = new System.Drawing.Point(787, 282);
+            this.save_btn.Name = "save_btn";
+            this.save_btn.Size = new System.Drawing.Size(251, 49);
+            this.save_btn.TabIndex = 3;
+            this.save_btn.Text = "Open Data File";
+            this.save_btn.UseVisualStyleBackColor = true;
+            this.save_btn.Click += new System.EventHandler(this.save_btn_Click);
+            // 
+            // value_pb
+            // 
+            this.value_pb.Location = new System.Drawing.Point(12, 33);
+            this.value_pb.Name = "value_pb";
+            this.value_pb.Size = new System.Drawing.Size(766, 23);
+            this.value_pb.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(785, 358);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Last value";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Status:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(797, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Port Name:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(863, 40);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(175, 21);
+            this.comboBox1.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(796, 80);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Baud Rate:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "14400",
+            "19200",
+            "38400",
+            "57600",
+            "115200"});
+            this.comboBox2.Location = new System.Drawing.Point(863, 72);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(175, 21);
+            this.comboBox2.TabIndex = 13;
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.chart1.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.Center;
+            this.chart1.BorderlineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Solid;
+            chartArea3.AlignmentOrientation = ((System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations)((System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations.Vertical | System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations.Horizontal)));
+            chartArea3.Area3DStyle.Inclination = 0;
+            chartArea3.Area3DStyle.LightStyle = System.Windows.Forms.DataVisualization.Charting.LightStyle.None;
+            chartArea3.Area3DStyle.Rotation = 0;
+            chartArea3.AxisX.LineColor = System.Drawing.Color.MidnightBlue;
+            chartArea3.AxisX.ScaleBreakStyle.Enabled = true;
+            chartArea3.AxisX.ScaleBreakStyle.MaxNumberOfBreaks = 3;
+            chartArea3.AxisX.ScaleBreakStyle.Spacing = 1D;
+            chartArea3.AxisX.Title = "Time";
+            chartArea3.AxisX.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            chartArea3.AxisY.LogarithmBase = 15D;
+            chartArea3.AxisY.Title = "Ozone in ppb";
+            chartArea3.AxisY.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            chartArea3.BackColor = System.Drawing.Color.AliceBlue;
+            chartArea3.BackImageAlignment = System.Windows.Forms.DataVisualization.Charting.ChartImageAlignmentStyle.Left;
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(18, 75);
+            this.chart1.Name = "chart1";
+            series3.BackSecondaryColor = System.Drawing.Color.Transparent;
+            series3.BorderColor = System.Drawing.Color.Transparent;
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Color = System.Drawing.Color.CornflowerBlue;
+            series3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold);
+            series3.IsValueShownAsLabel = true;
+            series3.LabelForeColor = System.Drawing.Color.CornflowerBlue;
+            series3.Legend = "Legend1";
+            series3.MarkerBorderColor = System.Drawing.Color.Blue;
+            series3.MarkerColor = System.Drawing.Color.Blue;
+            series3.MarkerSize = 3;
+            series3.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            series3.Name = "Ozone";
+            series3.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Time;
+            series3.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Single;
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(760, 363);
+            this.chart1.TabIndex = 14;
+            this.chart1.Text = "chart1";
+            title3.Name = "O3 in ppb/Time";
+            this.chart1.Titles.Add(title3);
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1067, 450);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.value_pb);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.save_btn);
+            this.Controls.Add(this.stop_btn);
+            this.Controls.Add(this.start_btn);
+            this.Controls.Add(this.data_tb);
+            this.ForeColor = System.Drawing.SystemColors.Desktop;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Serial Communication Ozone Sensor";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         public System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TextBox data_tb;
+        private System.Windows.Forms.Button start_btn;
+        private System.Windows.Forms.Button stop_btn;
+        private System.Windows.Forms.Button save_btn;
+        private System.Windows.Forms.ProgressBar value_pb;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        public System.Windows.Forms.Timer timer1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,24 +7,283 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO.Ports;
+using System.Globalization;
+using System.IO;
+using System.Diagnostics;
 
 namespace Port
 {
     public partial class Form1 : Form
     {
+        
+        int data2;
+        int data3;
+        int data4;
+        int data5;
+        int data6;
+        int data7;
+        int data8;
+        int data9;
+        int data10;
+        int data11;
+        int data12;
+        int data13;
+        int data14;
+        int data15;
+        int data16;
+        string combine;
+        float salida;
+        string receivedData;
+        Single salidabien;
+        DateTime dataahora;
+        String LinePort;
+        int LineBaud;
         public Form1()
         {
+
             InitializeComponent();
-            serialPort1.Open();
-        }
+            getAvailablePorts();
+            chart1.Series["Ozone"].IsValueShownAsLabel = true;
 
-        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        }
+        void getAvailablePorts()
         {
-            MessageBox.Show(serialPort1.ReadLine());
+            String[] ports= SerialPort.GetPortNames();
+            
+            comboBox1.Items.AddRange(ports);
         }
 
+        ///START-UP////////////////////////////////////
         private void Form1_Load(object sender, EventArgs e)
         {
+            timer1.Enabled = false;
+             int start = 1;
+             if(start == 1)
+             {
+               ///////Leer Configuración inicial Port/Bauds///////////////////////
+                try
+                {
+                    //Pass the file path and file name to the StreamReader constructor
+                    StreamReader sr = new StreamReader("Port.txt");
+                    
+                    //Read the first line of text
+                    LinePort = sr.ReadLine();
+                    comboBox1.Text = LinePort;
+                    String LineBaudS = sr.ReadLine();
+                    LineBaud = Int32.Parse(LineBaudS);
+                    comboBox1.Text = LinePort;
+                    comboBox2.Text = LineBaudS;
+                    data_tb.Text = "Wait 1 minute to load the data";
+                    sr.Close();
+
+
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Introduce the Start-up port and Bauds in the Port.txt");
+                }
+                finally
+                {
+                    Console.WriteLine("Conected to "+LinePort+" with "+LineBaud+" Baudrate");
+                   }
+                //////////////////////////////////////////////////////////
+              
+                 serialPort1.PortName = LinePort;
+                 serialPort1.Close();
+                 serialPort1.BaudRate = LineBaud;
+                 serialPort1.Parity = Parity.None;
+                 serialPort1.StopBits = StopBits.One;
+                 serialPort1.DataBits = 8;
+                 serialPort1.Handshake = Handshake.None;
+                 serialPort1.RtsEnable = false;
+
+                 serialPort1.DataReceived += new
+                     SerialDataReceivedEventHandler(port_DataReceived);
+                 serialPort1.Open();
+                 timer1.Enabled = true;
+                 value_pb.Value = 100;
+                 stop_btn.Enabled = true;
+                 start_btn.Enabled = false;
+                start = 0;
+                
+             }
+            
+        }
+        
+        /////////////////////////////////////
+      
+
+        private void start_btn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (comboBox1.Text == "" || comboBox2.Text == "")
+                {
+                    data_tb.Text = "Please select port settings";
+
+                }
+                else
+                {
+                    StreamWriter ss = new StreamWriter("Port.txt");
+
+                    ss.WriteLine(comboBox1.Text);
+                    ss.WriteLine(comboBox2.Text);
+                    ss.Close();
+                    serialPort1.PortName = comboBox1.Text;
+                    serialPort1.Close();
+                    serialPort1.BaudRate = Convert.ToInt32(comboBox2.Text);
+                    serialPort1.Parity = Parity.None;
+                    serialPort1.StopBits = StopBits.One;
+                    serialPort1.DataBits = 8;
+                    serialPort1.Handshake = Handshake.None;
+                    serialPort1.RtsEnable = false;
+                    
+                    serialPort1.DataReceived += new
+                        SerialDataReceivedEventHandler(port_DataReceived);
+                    serialPort1.Open();
+                    timer1.Enabled = true;
+                    value_pb.Value = 100;
+                    stop_btn.Enabled = true;
+                    start_btn.Enabled = false;
+
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                data_tb.Text = "Unauthorized Acess";
+            }
+        }
+
+        private void stop_btn_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+            timer1.Enabled = false;
+            value_pb.Value = 0;
+            start_btn.Enabled = true;
+            stop_btn.Enabled = false;
+
+        }
+        private void port_DataReceived(object sender,
+                                 SerialDataReceivedEventArgs e)
+        {
+           
+            data2 = serialPort1.ReadByte();
+            string hexdata2 = data2.ToString("X");
+            data3 = serialPort1.ReadByte();
+            string hexdata3 = data3.ToString("X");
+            data4 = serialPort1.ReadByte();
+            string hexdata4 = data4.ToString("X");
+            data5 = serialPort1.ReadByte();
+            string hexdata5 = data5.ToString("X");
+            data6 = serialPort1.ReadByte();
+            string hexdata6 = data6.ToString("X");
+            data7 = serialPort1.ReadByte();
+            string hexdata7 = data7.ToString("X");
+            data8 = serialPort1.ReadByte();
+            string hexdata8 = data8.ToString("X");
+            data9 = serialPort1.ReadByte();
+            string hexdata9 = data9.ToString("X");
+            data10 = serialPort1.ReadByte();
+            string hexdata10 = data10.ToString("X");
+            data11 = serialPort1.ReadByte();
+            string hexdata11 = data11.ToString("X");
+            data12 = serialPort1.ReadByte();
+            string hexdata12 = data12.ToString("X");
+            data13 = serialPort1.ReadByte();
+            string hexdata13 = data13.ToString("X");
+            data14 = serialPort1.ReadByte();
+            string hexdata14 = data14.ToString("X");
+            data15 = serialPort1.ReadByte();
+            string hexdata15 = data15.ToString("X");
+            data16 = serialPort1.ReadByte();
+            string hexdata16 = data16.ToString("X");
+            if (hexdata7.Length == 1)
+            {
+                hexdata7 = hexdata7.PadLeft(2, '0');    
+            }
+            if (hexdata6.Length == 1)
+            {
+                hexdata6 = hexdata6.PadLeft(2, '0');
+            }
+            if (hexdata5.Length == 1)
+            {
+                hexdata5 = hexdata5.PadLeft(2, '0');
+            }
+            if (hexdata4.Length == 1)
+            {
+                hexdata4 = hexdata4.PadLeft(2, '0');
+            }
+            combine = (hexdata7) + (hexdata6) + (hexdata5) + (hexdata4);
+            Int32 IntRep = Int32.Parse(combine, NumberStyles.AllowHexSpecifier);
+            salida = BitConverter.ToSingle(BitConverter.GetBytes(IntRep), 0);
+            salidabien = salida * 1000;
+            dataahora = DateTime.Now;
+            String SalidaData = salida + "," + dataahora;
+            File.AppendAllText("Data.txt", SalidaData + Environment.NewLine);
+
+            this.Invoke(new EventHandler(Showdata));
+
+        }
+
+        private void Showdata(object sender, EventArgs e)
+        {
+
+            double salida1 = Math.Round((salida * 1000),3);
+            data_tb.Text = salida1.ToString() + " ppb ";
+           /* if (data_tb.Text.Length > 13)
+            {
+                int foundS1 = data_tb.Text.IndexOf(" ");
+                int foundS2 = data_tb.Text.IndexOf(",",foundS1+1);
+                if (foundS1 != foundS2 && foundS1 >= 0)
+                {
+
+                    data_tb.Text = data_tb.Text.Remove(foundS1 + 1, foundS2 - foundS1);
+
+                    
+                }
+            }
+            */
+
+            dataahora = DateTime.Now;
+            Console.WriteLine(dataahora);
+            chart1.Series["Ozone"].Points.AddXY(dataahora, Math.Round(salidabien, 2));
+            if (chart1.Series["Ozone"].Points.Count > 20) { 
+            chart1.Series["Ozone"].Points.RemoveAt(0);
+                chart1.ResetAutoValues();
+            }    
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) 
+        {
+            receivedData = combine;
+          
+        }
+
+        private void save_btn_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText("DataForCsv.txt", File.ReadAllText("Headers.txt") + Environment.NewLine+File.ReadAllText("Data.txt"));
+            Process.Start("notepad.exe", "DataForCsv.txt");
 
         }
     }
